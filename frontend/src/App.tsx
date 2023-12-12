cf9ede77efdd8d81b3c4707d7d7e9929b1e497b5
Alper Erdogan <alpererdogan8@gmail.com>
2023-12-12 20:43:31 +0300

    feat: edited files according to config settings

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,27 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
-
+import { useTheme } from "./context/useTheme";
 function App() {
-  const [count, setCount] = useState(0);
-
+  const { setTheme, theme } = useTheme();
   return (
-    <>
+    <div className="flex flex-col justify-center items-center h-[100dvh] font-sans text-2xl">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={viteLogo} className="logo w-28 m-5" alt="Vite logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button className="text-slate-950 bg-orange-400" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="card text-center">
+        <button
+          className="text-primary-foreground bg-primary rounded-xl py-2 px-10 m-5 bg-orange-400"
+          onClick={() => setTheme(theme !== "dark" ? "dark" : "light")}>
+          {theme !== "dark" ? "dark" : "light"}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    </div>
   );
 }
 
