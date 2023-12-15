d978773deab16502b6053c319e48bf23c65a7b8e
Alper Erdogan <alpererdogan8@gmail.com>
2023-12-15 14:54:30 +0300

    feat: Added Theme and Auth providers

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ThemeProvider } from "./context/useTheme.tsx";
+import { ThemeProvider } from "./context/theme-provider.tsx";
+import { AuthProvider } from "./context/auth-provider.tsx";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="theme">
-      <App />
-    </ThemeProvider>
+    <AuthProvider>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="light" storageKey="theme">
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>,
 );
