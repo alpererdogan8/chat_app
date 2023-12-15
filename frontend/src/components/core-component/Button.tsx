2ccc57b3b3f445a0278a5e1332115687f4dabcfe
Alper Erdogan <alpererdogan8@gmail.com>
2023-12-15 21:16:54 +0300

    fix(style): icon ratio fixed

--- frontend/src/components/core-component/Button.tsx
+++ frontend/src/components/core-component/Button.tsx
@@ -21,7 +21,7 @@ const variantsButton = cva("inline-flex items-center justify-center button round
       default: "h-10 px-4 py-2",
       sm: "h-9 rounded-md px-3",
       lg: "h-11 rounded-md px-8",
-      icon: "h-10 w-10",
+      icon: "h-[43px] w-[48px]",
     },
   },
   defaultVariants: {
