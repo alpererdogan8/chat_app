d238d03be547f8ede5642a054f3a41cfba9de71d
Alper Erdogan <alpererdogan8@gmail.com>
2023-12-15 14:59:45 +0300

    feat(login-page):components created in login page

---
+++ frontend/src/pages/Login/LoginPage.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { LoginFormPage } from "./LoginFormPage";
+import { LoginImagePage } from "./LoginImagePage";
+
+export const LoginPage = () => {
+  return (
+    <div className="w-full h-[100dvh] flex  items-center justify-between">
+      <LoginImagePage />
+      <LoginFormPage />
+    </div>
+  );
+};
