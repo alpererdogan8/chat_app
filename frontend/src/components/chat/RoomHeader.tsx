6fc34b65cde413c4b70aa7ebdeb054a4278d6c05
Alper Erdogan <alpererdogan8@gmail.com>
2023-12-16 17:47:25 +0300

    fix(style): change height

--- frontend/src/components/chat/RoomHeader.tsx
+++ frontend/src/components/chat/RoomHeader.tsx
@@ -5,7 +5,7 @@ export const RoomHeader = ({ isFounder }: { isFounder: boolean }) => {
   return (
     <>
       <Cards className="flex flex-col">
-        <div className="w-full  py-6 px-2.5 items-center h-[91px]">
+        <div className="w-full  py-6 px-2.5 items-center h-[94px]">
           <Cards.Header className="font-semibold ml-3 gap-3 flex justify-center text-sm lg:text-base py-2.5 lg:py-2">
             <div contentEditable={isFounder}> Alper Erdoğan</div>{" "}
             {isFounder && (
