066d09cea44d8d5eed363c6bcee3953ef4c9c5b2
Alper Erdogan <alpererdogan8@gmail.com>
2023-12-18 00:56:15 +0300

    feat:added services

---
+++ backend/services/AuthService.ts
@@ -0,0 +1,46 @@
+import { Document, Error, Model, PassportLocalDocument, PassportLocalModel, SaveOptions } from "mongoose";
+import MainService from "./MainService";
+import User from "../database/models/User";
+import passportConfig from "../config/passportConfig";
+import { Request, Response, NextFunction } from "express";
+type SignupType = {
+  username: string;
+  email: string;
+  password: string;
+};
+
+class AuthService extends MainService {
+  initialize: any;
+  session: any;
+  constructor(model: any) {
+    super(model);
+    this.initialize = passportConfig.initialize();
+    this.session = passportConfig.session();
+  }
+  public async signup({ username, email, password }: SignupType): Promise<any> {
+    try {
+      const userExist: any = await this.findBy("email", email);
+      if (userExist) throw { message: "Email already exists", status: 409 };
+      if (userExist === null) {
+        const newUser = new this.model({
+          email,
+          username,
+        }) as PassportLocalDocument;
+        await newUser.setPassword(password);
+        await newUser.save();
+        return newUser;
+      }
+    } catch (error) {
+      console.error(error);
+      return error;
+    }
+  }
+  login() {
+    return passportConfig.authenticate("local");
+  }
+  isOnline(state: boolean) {
+    return this.model.updateOne({ isOnline: state });
+  }
+}
+
+export default new AuthService(User);
