dc8444fb02613a469b0b7160d038e2094e8a2dbc
Alper Erdogan <alpererdogan8@gmail.com>
2023-12-18 14:56:53 +0300

    fix: fixed bugs in type edits

--- backend/services/MainService.ts
+++ backend/services/MainService.ts
@@ -1,42 +1,41 @@
 import { Document, Model, FilterQuery, UpdateQuery } from "mongoose";
 import { IUserModal } from "../database/models/User";
 
-
-class MainService {
-  constructor(protected model: Model<Document>) {}
+class MainService<M> {
+  constructor(protected modal: Model<Document>) {}
 
   load(): Promise<Document[]> {
-    return this.model.find().exec();
+    return this.modal.find();
   }
 
   async insert(obj: object): Promise<Document> {
-    return await this.model.create(obj);
+    return await this.modal.create(obj);
   }
 
-  async removeTo(property: string, value: any): Promise<any> {
+  async removeTo<T extends keyof M>(property: T, value: any): Promise<any> {
     const filter: FilterQuery<Document> = { [property]: value };
-    return this.model.deleteOne(filter);
+    return this.modal.deleteOne(filter);
   }
 
   async update(id: string, obj: UpdateQuery<Document>): Promise<any> {
-    return this.model.findByIdAndUpdate(id, obj);
+    return this.modal.findByIdAndUpdate(id, obj).exec();
   }
 
   async find(id: string): Promise<Document | null> {
-    return this.model.findById(id).exec();
+    return this.modal.findById(id).exec();
   }
 
-  async findBy<T extends keyof IUserModal>(property: T, value: IUserModal[T]): Promise<Document | null> {
+  async findBy<T extends keyof M>(property: T, value: any): Promise<Document | null> {
     const filter: FilterQuery<Document> = { [property]: value };
-    return this.model.findOne(filter).exec();
+    return this.modal.findOne(filter).exec();
   }
 
   async delete(id: string): Promise<any> {
-    return this.model.findByIdAndDelete(id);
+    return this.modal.findByIdAndDelete(id);
   }
 
   async query(obj: object): Promise<Document[]> {
-    return this.model.find(obj).exec();
+    return this.modal.find(obj).exec();
   }
 }
 
