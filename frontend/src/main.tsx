8025002e5619c6d6c058b832aae18ad755c8c24b
Alper Erdogan <alpererdogan8@gmail.com>
2023-12-20 02:25:49 +0300

    feat: added message provider and changed provider rankings

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,16 +4,22 @@ import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "./context/theme-provider.tsx";
 import { AuthProvider } from "./context/auth-provider.tsx";
+import { RoomsProvider } from "./context/chat/rooms-provider.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { MessageProvider } from "./context/chat/messages-provider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <BrowserRouter>
-        <ThemeProvider defaultTheme="light" storageKey="theme">
-          <App />
-        </ThemeProvider>
-      </BrowserRouter>
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <RoomsProvider>
+          <MessageProvider>
+            <ThemeProvider defaultTheme="light" storageKey="theme">
+              <App />
+            </ThemeProvider>
+          </MessageProvider>
+        </RoomsProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
