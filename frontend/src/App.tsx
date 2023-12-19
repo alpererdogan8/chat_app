46be7ac289fe65a0251285d01c3a9cdf6eb01eda
Alper Erdogan <alpererdogan8@gmail.com>
2023-12-20 02:26:33 +0300

    feat: secure route provided with auth provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,24 @@
+import { useAuth } from "./context/auth-provider";
+import { LoginPage } from "./pages/Login/LoginPage";
 import { ChatPage } from "./pages/chat/ChatPage";
-import { LoginPage } from "./pages/login/LoginPage";
+
 import { Navigate, Outlet, Route, RouteProps, Routes } from "react-router-dom";
 
-const isAuthenticated = () => {
-  return true;
-};
 const PrivateRoute: React.FC<RouteProps> = () => {
-  return isAuthenticated() ? <Outlet /> : <Navigate to="/login" replace={true} />;
+  const { isAuthenticated } = useAuth();
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace={true} />;
 };
 
 function App() {
+  const { isAuthenticated } = useAuth();
+
   return (
     <Routes>
       <Route path="/" element={<PrivateRoute />}>
         <Route index element={<Navigate to="/chat" />} />
         <Route path="chat" element={<ChatPage />} />
       </Route>
-      <Route path="login" element={isAuthenticated() ? <Navigate to="/" /> : <LoginPage />} />
+      <Route path="login" element={isAuthenticated ? <Navigate to="/" /> : <LoginPage />} />
     </Routes>
   );
 }
