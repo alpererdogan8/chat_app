cca576d01e9e84482b343354b8c1e329ddfc9cab
Alper Erdogan <alpererdogan8@gmail.com>
2023-12-20 02:36:16 +0300

    feat: backend connection provided for past messaging

--- frontend/src/components/chat/Chat.tsx
+++ frontend/src/components/chat/Chat.tsx
@@ -1,20 +1,50 @@
+import { useMessage } from "@/context/chat/messages-provider";
 import { MessageBox } from "./MessageBox";
+import { useEffect } from "react";
+import { useAuth } from "@/context/auth-provider";
+
+interface Message {
+  _id: string;
+  admin: {
+    _id: string;
+  };
+  sender: {
+    _id: string;
+    username: string;
+    email: string;
+    isOnline: boolean;
+    timestamp: string;
+  };
+  message: string;
+  timestamp: string;
+  wasItSentByHimself: boolean;
+}
 
 export const Chat = () => {
+  const { messageData } = useMessage();
+  const { profile } = useAuth();
+  useEffect(() => {
+    console.log(messageData.sender?.username);
+  }, [messageData]);
+
   return (
     <div className=" h-[68dvh] overflow-auto flex flex-col m-2 px-4 py-5 gap-2 ">
-      <MessageBox postedBy={true} />
-      <MessageBox postedBy={true} />
-      <MessageBox postedBy={false} />
-      <MessageBox postedBy={true} />
-      <MessageBox postedBy={false} />
-      <MessageBox postedBy={true} />
-      
-      <MessageBox postedBy={false} />
-      <MessageBox postedBy={false} />
-      <MessageBox postedBy={false} />
-      <MessageBox postedBy={true} />
-      <MessageBox postedBy={false} />
+      {messageData._id ? (
+        messageData?.messages.map((message: Message) => {
+          return (
+            <>
+              <MessageBox
+                message={message.message}
+                timestamp={message.timestamp}
+                sender={message.sender.username}
+                wasItSentByHimself={message?.sender._id === profile.user?._id}
+              />
+            </>
+          );
+        })
+      ) : (
+        <div className="w-full h-[100dvh] flex justify-center items-center text-5xl"> ⬅️ Enter the Rooms</div>
+      )}
     </div>
   );
 };
